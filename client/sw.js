b5597f5d2d7f945484a32fb828584d35081882cd
Tigh Gallagher <109920323+KingTigh@users.noreply.github.com>
2025-10-21 14:05:49 -0400

    Delete client directory

--- client/sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='finance-v1';
-const urls=['','index.html','style.css','app.js','manifest.json','icon-192.png','icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(urls))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
